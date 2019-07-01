9db908dcc48e51a14e9d21b2d9f6242db67fa0ad
Hyungsoo Kim <javaos74@gmail.com>
2019-07-01 10:00:31 +0900

    Update webhook.js
    프로세스 실행  관련 프로세스 이름 및 로봇 이름 추가

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var MY_TENANT_ID = 77469;
 var MY_QUEUE = 'TestQueue';
 
+//사용가능한 Orchestrator 및 테넌트 정보로 변경 필요 
 var orch = new Orchestrator('koreatogether', 'admin', 'password', 'https://platform.uipath.com/koreatogether/koreatogether/')
 
 
@@ -19,8 +20,8 @@ router.post('/', function(req, res, next) {
     if( tenant == MY_TENANT_ID) {
         if( type == 'queueItem.added' && req.body['Queue']['Name'] == MY_QUEUE) {
             //start new job hello world 
-            let relKey = orch.getReleaseKey('사용자정보출력');
-            let robotId = orch.getRobotId('개발로봇');
+            let relKey = orch.getReleaseKey('사용자정보출력'); // 해당 Tenant에 정의한 프로세스 이름 
+            let robotId = orch.getRobotId('개발로봇'); // 로봇 이름 
             console.log('startJob called');
             let job = orch.startJob( { startInfo: {
                                     ReleaseKey : `${relKey}`,
