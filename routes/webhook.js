e319f6901828ca861cff0de96f28b41cdc3b1f5c
Hyungsoo Kim <javaos74@gmail.com>
2019-07-12 13:55:03 +0900

    schedule Job 이 끝나면 Job 상태를 전달하는 새로운 Job 실행 코드 추가

--- routes/webhook.js
+++ routes/webhook.js
@@ -55,30 +55,5 @@ router.post('/', function(req, res, next) {
 });
 
 
-/* POST log webhook request . 
-router.post('/', function(req, res, next) {
-    console.log(req.body)
-    res.sendStatus(202);
-
-    let type = req.body['Type']
-    let tenant = req.body['TenantId']
-    if( tenant == MY_TENANT_ID) {
-        if( type == 'queueItem.added' && req.body['Queue']['Name'] == MY_QUEUE) {
-            //start new job hello world 
-            let relKey = orch.getReleaseKey('사용자정보출력'); // 해당 Tenant에 정의한 프로세스 이름 
-            let robotId = orch.getRobotId('개발로봇'); // 로봇 이름 
-            console.log('startJob called');
-            let job = orch.startJob( { startInfo: {
-                                    ReleaseKey : `${relKey}`,
-                                    Strategy: 'Specific',
-                                    RobotIds: [ parseInt(`${robotId}`)],
-                                    Source: 'Manual'
-				                } });
-
-	   console.log( job);
-        }
-    }
-});
-*/
 
 module.exports = router;
